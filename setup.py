d0abf5ea2400b26fb8fda1b0066ea6471ea9ecb0
Mattia Ibba <mattiaibba90@hotmail.it>
2021-04-24 00:04:52 +0200

    first version of setup.py working

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
-import platform
-#import setuptools
-import subprocess
-import sys
-import getopt
+from setuptools import setup, find_packages
+from setuptools.command.install import install
 
 #TODO this for the PIP install manual
 try:
@@ -11,45 +8,44 @@ except ImportError:
     from pip._internal import main as pipmain
 
 #TODO add packages=["diambra"], depending on the installation and python_requires='>3.6'
-#from setuptools.command.install import install
-#class DiambraInstall(install):
-#    user_options = install.user_options + [
-#        ('stablebaseline', None, 'Core or stablebaseline installation')
-#    ]
-#
-#    def initialize_options(self):
-#        install.initialize_options(self)
-#        self.stablebaseline = 0
-#
-#    def finalize_options(self):
-#        install.finalize_options(self)
-#
-#    def run(self):
-#        stablebaseline = self.stablebaseline
-#        install.run(self)
+class DiambraInstall(install):
+    user_options = install.user_options + [
+        ('stablebaseline', None, 'Core or stablebaseline installation')
+    ]
 
-arg=sys.argv[1:]
-if(arg == "stablebaseline"):
-    if(sys.version_info < (3, 6)):
-        print("Warning, update your Python version to 3.6 at least to use stablebaseline")
-        sys.exit()
+    def initialize_options(self):
+        install.initialize_options(self)
+        self.stablebaseline = 0
+
+    def finalize_options(self):
+        install.finalize_options(self)
+
+    def run(self):
+        stablebaseline = self.stablebaseline
+        install.run(self)
+
+#arg=sys.argv[1:]
+#if(arg == "stablebaseline"):
+#    if(sys.version_info < (3, 6)):
+#        print("Warning, update your Python version to 3.6 at least to use stablebaseline")
+#        sys.exit()
 
 
-platform=platform.system()
-if(platform != "Linux"):
-    print("Warning, only Linux platforms are supported for this package")
+#platform=platform.system()
+#if(platform != "Linux"):
+#    print("Warning, only Linux platforms are supported for this package")
 
-subprocess.run("./setupOS.sh")
-pipmain(['install', 'distro'])
-import distro
+#subprocess.run("./setupOS.sh")
+#pipmain(['install', 'distro'])
+#import distro
 
-distro=distro.linux_distribution()
-name=distro[0]
-release=float(distro[1])
+#distro=distro.linux_distribution()
+#name=distro[0]
+#release=float(distro[1])
 
-if(not(name == "Ubuntu") and not(name == "Linux Mint")):
-    print("Warning, only Ubuntu and Mint distros are supported for this package")
-    sys.exit()
+#if(not(name == "Ubuntu") and not(name == "Linux Mint")):
+#    print("Warning, only Ubuntu and Mint distros are supported for this package")
+#    sys.exit()
 
 
 #NOTE This is the python-version of the script setupOS.sh #TODO to substitute with the option on setup (console_entry)
@@ -83,7 +79,13 @@ if(not(name == "Ubuntu") and not(name == "Linux Mint")):
 with open("README.md", "r") as description:
     long_description = description.read()
 
+#TODO Add a script for Diambra?
+#entry_points={
+#        'console_scripts': ['my-command=exampleproject.example:main']
+#    }
+
 #NOTE This is the true creation of the setup env
+#TODO From install add informations on package_data in order to get only stable_baselines info
 setuptools.setup(
         name='DIAMBRAEnvironment',
         url='https://github.com/diambra/DIAMBRAenvironment',
@@ -101,7 +103,8 @@ setuptools.setup(
             'jupyter>=1.0.0',
             'opencv-contrib-python>=4.4.0.42',
             'opencv-python>=4.4.0.42'],	
-        find_packages(include=['diambra_environment', 'diambra_environment/customPolicies', 'diambra_environment/mame', 'diambra_environment/utils', 'diambra_environment/diambraEnvLib', 'examples', 'img', 'roms']),
+        packages=find_packages(include=['diambra_environment', 'examples', 'diambra_environment/customPolicies', 'diambra_environment/utils']),
+        package_data={'diambra_environment': ['diambra_environment/mame/mame.zip', 'diambra_environment/diambraEnvLib/*.so', 'img/*', 'roms/integratedGames.json', 'examples/core/*.ipynb', 'examples/stable_baselines/*.ipynb']},
         classifiers=['Operating System :: Ubuntu 18.04 :: Ubuntu 20.04 :: Mint 19 Cinnamon :: Mint 20 Ulysse'],
         cmdclass={'install': DiambraInstall}
         )
