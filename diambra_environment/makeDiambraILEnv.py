6ca7a0c015ab7ef152b4c01db75a68f1c124d33a
Alessandro Palmas <alessandropalmas.mail@gmail.com>
2021-05-08 16:14:35 +0200

    Completed fixes for core. SB to be tested

--- diambra_environment/makeDiambraILEnv.py
+++ diambra_environment/makeDiambraILEnv.py
@@ -7,6 +7,6 @@ def make_diambra_imitationLearning_env(diambraIL, diambraIL_kwargs, seed=0,
     """
 
     assert diambraIL_kwargs["totalCpus"] == 1, "Only single proc environments can be created with this function, for a ready to use multiproc execution use the version based on StableBaselines"
-    env = diambraIL(**diambraIL_kwargs, rank=rank)
+    env = diambraIL(**diambraIL_kwargs, rank=0)
 
     return env
