63b3d3c1fe223c0d815868fec885adc50786d1a0
Johannes 'fish' Ziemke <github@5pi.de>
2022-06-15 14:41:04 +0000

    Add classifiers

--- setup.py
+++ setup.py
@@ -46,5 +46,16 @@ setuptools.setup(
         'grpcio-tools'],
     packages=[package for package in find_packages() if package.startswith("diambraArena")],
     include_package_data=True,
-    extras_require=extras
+    extras_require=extras,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Scientific/Engineering :: Artificial Life',
+        'Topic :: Games/Entertainment',
+        'Topic :: Games/Entertainment :: Arcade',
+        'Topic :: Education',
+    ]
 )
