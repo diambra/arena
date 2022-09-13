7c3d19df6024ee034d2df90bc36950a60be226be
Johannes Ziemke <github@5pi.de>
2022-09-13 17:10:50 +0200

    Remove __version__ override from __init__.py

--- diambra/arena/__init__.py
+++ diambra/arena/__init__.py
@@ -1,4 +1,3 @@
 from .make_env import make
 from .arena_imitation_learning_gym import ImitationLearning, ImitationLearningHardcore
 from .utils.gym_utils import available_games, game_sha_256, check_game_sha_256
-__version__ = '2.0a0'
