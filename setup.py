3a7bf4d96f3aa42807073a17a463061fcaea36be
alexpalms <alessandropalmas.mail@gmail.com>
2023-02-04 11:11:39 -0500

    Added limit on diambra-engine (need to make sure grpc versions match)

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setuptools.setup(
             'opencv-contrib-python>=4.4.0.42',
             'opencv-python>=4.4.0.42',
             'grpcio',
-            'diambra-engine',
+            'diambra-engine>2.2.x',
             'dacite'],
         packages=[package for package in setuptools.find_packages(
         ) if package.startswith("diambra")],
