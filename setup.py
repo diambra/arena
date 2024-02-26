bc8b63a21d4622aff793b43a3c708d6d4dd10d3e
Michele Milesi <michele.milesi@studio.unibo.it>
2024-02-26 15:22:37 +0100

    feat: update sheeprl to v0.5.4

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ extras = {
     "stable-baselines3": ["stable-baselines3[extra]~=2.1.0", "pyyaml"],
     "ray-rllib": ["ray[rllib]~=2.7.0", "tensorflow", "torch", "pyyaml"],
     "sheeprl": [
-        "sheeprl==0.5.1",
+        "sheeprl==0.5.4",
         "importlib-resources==6.1.0",
     ],
 }
